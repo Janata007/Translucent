5eceac89a25ba9795247e6dbfd2fd4c8238fe525
Jana Markovikj <jana.markovikj@endava.com>
2022-11-07 20:14:26 +0100

    checkstyle

--- sector-service/src/main/java/com/example/sectorservice/controller/SectorController.java
+++ sector-service/src/main/java/com/example/sectorservice/controller/SectorController.java
@@ -2,7 +2,12 @@ package com.example.sectorservice.controller;
 
 import com.example.sectorservice.entity.Sector;
 import com.example.sectorservice.service.SectorService;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/sector")
@@ -17,8 +22,9 @@ public class SectorController {
     public Sector saveSector(@RequestBody Sector sector) {
         return sectorService.save(sector);
     }
+
     @GetMapping("/{id}")
-    public Sector findSectorById(@PathVariable Long id){
+    public Sector findSectorById(@PathVariable Long id) {
         return sectorService.findSectorById(id);
     }
 }
