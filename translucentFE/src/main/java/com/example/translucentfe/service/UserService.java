e5a693b2104d8d57f1beab757da19e5511c3cddc
Jana Markovikj <jana.markovikj@endava.com>
2022-12-16 12:54:06 +0100

    remove old FE app

--- translucentFE/src/main/java/com/example/translucentfe/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.example.translucentfe.service;
-
-import com.example.translucentfe.model.NewUser;
-import com.example.translucentfe.model.UserWithSectorVO;
-
-public interface UserService {
-    UserWithSectorVO getUserWithSector(Long userId);
-    NewUser registerUser(NewUser user);
-}
