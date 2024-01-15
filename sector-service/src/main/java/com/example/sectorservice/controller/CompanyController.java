63a1eba6cb239811c750b98db3208acca61d76ff
Jana Markovikj <jana.markovikj@endava.com>
2024-01-15 10:38:21 +0100

    cors error handling

--- sector-service/src/main/java/com/example/sectorservice/controller/CompanyController.java
+++ sector-service/src/main/java/com/example/sectorservice/controller/CompanyController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,6 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/company")
+@CrossOrigin
 public class CompanyController {
     @Autowired
     private CompanyServiceImplementation companyService;
