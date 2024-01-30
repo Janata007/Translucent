0f015ea2659367300e0eff335444f147e495926b
Jana Markovikj <jana.markovikj@endava.com>
2024-01-30 10:23:53 +0100

    crossorigin fix

--- user-service/src/main/java/com/example/userservice/controller/ArrangementController.java
+++ user-service/src/main/java/com/example/userservice/controller/ArrangementController.java
@@ -9,6 +9,7 @@ import java.util.Arrays;
 import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/arrangements")
 @Slf4j
+@CrossOrigin
 public class ArrangementController {
     @Autowired
     private UserServiceImpl userService;
